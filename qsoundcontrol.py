068cd369f363eb691961fc518f6572004e2413a5
Eddy Petrișor <eddy.petrisor@gmail.com>
2013-02-09 02:00:25 +0200

    use PySide when appropriate and fix some issues

--- qsoundcontrol.py
+++ qsoundcontrol.py
@@ -1,17 +1,26 @@
+from PySide import QtCore, QtGui
 
-class QSoundControl(QWidget):
+from ui_soundcontrol import Ui_QSoundControl
+
+class QSoundControl(QtGui.QWidget):
 
     def __init__(self, parent=None, name='soundxxx', mp3=None, flac=None):
         self._name = name
         self.parent = parent
+
+        QtGui.QWidget.__init__(self, parent)
+
         if parent:
             if parent.hasName(self.name):
                 self.name = parent.getNewName(self.name)
             parent.register(self, self.name)
 
+        self.ui = Ui_QSoundControl()
+        self.ui.setupUi(self)
+
     def __del__(self):
-        if parent:
-            parent.unregister(self, self.name)
+        if self.parent:
+            self.parent.unregister(self, self.name)
 
     #TODO: add actual code for QSoundControl
 
