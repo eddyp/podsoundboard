acf99e75c8dd990637887ded046ce3ab6928577b
Eddy Petrișor <eddy.petrisor@gmail.com>
2013-02-09 12:45:55 +0200

    add sound registering and unregistering mechanism

--- mainwindow.py
+++ mainwindow.py
@@ -9,10 +9,31 @@ class myMainWindow(QtGui.QMainWindow):
     def __init__(self, parent=None):
         QtGui.QMainWindow.__init__(self, parent)
 
+        sounds = []
+        qsndctls = []
+        #TODO: init from app config files
+        #TODO: add profiles
         self.ui = Ui_MainWindow()
 
         self.ui.setupUi(self)
-        
+
+    def hasSound(self, name):
+        return (name in sounds)
+
+    def getNewSoundName(self, name):
+        #TODO: find a smart way to compute a new unique name        
+
+        #TODO: this might fail to provide a good name when
+        #      other than the last sound is removed
+        return 'name%d' % (len(sounds)+1)
+
+    def register(self, qsndctl, name):
+        if not hasSound(name):
+            self.sounds.append(name)
+            self.qsndctls.append( {'name': name, 'ctl': qsndctl} )
+        else:
+            raise AttributeError("Name %s already exists" % name)
+
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
